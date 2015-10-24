a32889e11fee2d78f13c5d9957bed51b4acc9781
Uinstyn <Auraoic@gmail.com>
2015-10-24 16:35:05 -0400

    Made textures for people

--- src/game/TestObject.java
+++ src/game/TestObject.java
@@ -4,7 +4,7 @@ public class TestObject extends GObject implements Renderable
 {
 	
 	public TestObject(float x, float y, float z) {
-		super(x, y, z, 0.03f, "icecreamrotate.png", "icecream.obj");
+		super(x, y, z, 0.01f, "friezaRotate.png", "man001.obj");
 		// TODO Auto-generated constructor stub
 	}
 	
