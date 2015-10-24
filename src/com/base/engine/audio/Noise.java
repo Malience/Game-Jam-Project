2e7d93edf570653dc5e326cdeeb7ad3b3d2eb90e
Haru <jaclynwoodlief@gmail.com>
2015-10-24 18:34:30 -0400

    ♪♫♪

---
+++ src/com/base/engine/audio/Noise.java
@@ -0,0 +1,28 @@
+package com.base.engine.audio;
+
+
+	import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+public class Noise
+{
+	public void play(){
+		
+		File music = new File("res/audio/ujam(1).wav");
+		
+	
+	 try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(music.getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
+}
