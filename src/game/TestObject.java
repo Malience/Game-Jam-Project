fe08a61e02b8f6dd8ed456c0ca148cfa3da1e238
Uinstyn <Auraoic@gmail.com>
2015-10-24 05:06:58 -0400

    I changed things

--- src/game/TestObject.java
+++ src/game/TestObject.java
@@ -4,7 +4,7 @@ public class TestObject extends GObject implements Renderable
 {
 	
 	public TestObject(float x, float y, float z) {
-		super(x, y, z, 0.1f, "orange.png", "orange.obj");
+		super(x, y, z, 0.1f, "colortest.png", "testcolors.obj");
 		// TODO Auto-generated constructor stub
 	}
 	
