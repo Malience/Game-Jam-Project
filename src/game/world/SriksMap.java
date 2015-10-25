982f267afad9ad7c77c201d9bc5650535a7f5bfa
MasterMatthew <Mastamats@yahoo.com>
2015-10-24 20:44:24 -0400

    ewtbw

--- src/game/world/SriksMap.java
+++ src/game/world/SriksMap.java
@@ -8,6 +8,6 @@ public class SriksMap extends subgame
 		 * Create objects such as Walls by using their constructor
 		 * Edit them however you'd like
 		 * Then put them into the game with World.New(Object);
-		 */
+		 */ 
 	}
 }
