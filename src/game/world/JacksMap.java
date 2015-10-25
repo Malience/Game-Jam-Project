b58dea32ab1544038a0577fd074b3ef4e0127b04
Haru <jaclynwoodlief@gmail.com>
2015-10-24 22:20:50 -0400

    left and bottom walls

--- src/game/world/JacksMap.java
+++ src/game/world/JacksMap.java
@@ -15,20 +15,37 @@ public class JacksMap extends subgame
 		 */
 		
 		//Top wall
-		Wall lWall;
+		Wall tWall;
 		for(double i = -9.5; i <= 30; i++)
 		{
-			lWall = new Wall((float)(i+0.1),(float)(0),(float)(29.9));
-			World.New(lWall);
+			tWall = new Wall((float)(i+0.1),(float)(0),(float)(29.9));
+			World.New(tWall);
 		}
 		
 		//right wall
-		Wall tWall = new Wall((float)1.0,(float)0.0,(float)1.0);		
+		Wall rWall = new Wall((float)1.0,(float)0.0,(float)1.0);		
 		for(double i = -10.0; i <= 30; i++)
 		{
-			tWall = new Wall((float)(30.0),(float)(0.0),(float)(i+0.1));
-			tWall.setRotY((float)2.0015);
-			World.New(tWall);
+			rWall = new Wall((float)(30.0),(float)(0.0),(float)(i+0.1));
+			rWall.setRotY((float)2.0015);
+			World.New(rWall);
 		}
+		
+		//bottom wall
+		Wall bWall;
+		for(double i = -9.5; i <= 30; i++)
+		{
+			bWall = new Wall((float)(i+0.1),(float)(0),(float)(-10.0));
+			World.New(bWall);
+		}
+		
+		//left wall
+				Wall lWall = new Wall((float)1.0,(float)0.0,(float)1.0);		
+				for(double i = -10.0; i <= 30; i++)
+				{
+					lWall = new Wall((float)(-10.0),(float)(0.0),(float)(i+0.1));
+					lWall.setRotY((float)2.0015);
+					World.New(lWall);
+				}
 	}
 }
