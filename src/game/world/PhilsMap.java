d62667e6c751bd22f45d7c09f8a68384f2b4393b
bellum128 <bellum128@gmail.com>
2015-10-25 12:47:22 -0400

    Gave myself a useful function.

--- src/game/world/PhilsMap.java
+++ src/game/world/PhilsMap.java
@@ -1,5 +1,6 @@
 package game.world;
 
+import game.GObject;
 import game.objects.*;
 
 public class PhilsMap extends subgame
@@ -11,10 +12,16 @@ public class PhilsMap extends subgame
 		 * Edit them however you'd like
 		 * Then put them into the game with World.New(Object);
 		 */
-		World.New(new Table(3,0,0));
-		DrinkMachine dm = new DrinkMachine(3,0.2f,-0.05f);
-		dm.setRotY(-180);
 		
-		World.New(dm);
+		PhilsMap.addToWorldWithRotation(new Table(3,0,0), 0, 0, 0);
+		PhilsMap.addToWorldWithRotation(new DrinkMachine(3,0.2f,-0.05f), 0, -180, 0);
+	}
+	
+	public static void addToWorldWithRotation(GObject f, float pitch, float yaw, float roll)
+	{
+		f.setRotX(pitch);
+		f.setRotY(yaw);
+		f.setRotZ(roll);
+		World.New(f);
 	}
 }
