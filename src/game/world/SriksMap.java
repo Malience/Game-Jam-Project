bff844458b78c59cb8420cca61e771acaad75143
Haru <jaclynwoodlief@gmail.com>
2015-10-25 16:37:21 -0400

    cleaning up a bit

--- src/game/world/SriksMap.java
+++ src/game/world/SriksMap.java
@@ -13,13 +13,7 @@ public class SriksMap extends subgame
 		 * Edit them however you'd like
 		 * Then put them into the game with World.New(Object);sa
 		 */ 
-		
-		//World.New(new Table (1f, 0f, 1f));
-		//World.New(new Table (2f, 0f, 2f));
-		//World.New(new Table (3f, 0f, 3f));
-		//World.New(new Table (-2f, 0f, 5f));
-		//World.New(new Table (3f, 0f, -3f));
-		//World.New(new Table (5f, 0f, -2f));
+
 
 		TestObject t = new TestObject(0f, 0, 1f);
 		t.setScale(.001f);
@@ -28,22 +22,8 @@ public class SriksMap extends subgame
 		World.New(new Counter(0f, 0f, 5f));
 		World.New(new Counter(.3f, 0f, 5f));
 		
-		//World.New(new Table (-8f, 0f, 8f));
 		World.New(new Table (-6f, 0f, 6f));
-		//World.New(new Table (-9f, 0f, 9f));
-		//World.New(new Table (5f, 0f, -3f));
-		//World.New(new Table (5f, 0f, -5f));
-		//World.New(new Table (3f, 0f, 3f));
-		
-		//TestObject t = new TestObject(0f, 0, 1f);
-		//t.setScale(.001f);
-		//World.New(t);
-		
 
-		
-
-		//World.New(new Table (1f, 0f, 1f));
-		//World.New(new Table (1f, 0f, 1f));
 		
 	}
 }
