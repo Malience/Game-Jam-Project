386d5262206740f1d5a57c704e6fee547bc23cb6
Uinstyn <Auraoic@gmail.com>
2015-10-24 22:52:59 -0400

    I made things

--- src/game/TestObject.java
+++ src/game/TestObject.java
@@ -6,8 +6,8 @@ public class TestObject extends GObject implements Renderable
 	public TestObject(float x, float y, float z) {
 		super(x, y, z);
 		setScale(0.01f);
-		setTexture("friezaRotate.png");
-		setMesh("man001.obj");
+		setTexture("fog2.png");
+		setMesh("FOG.obj");
 	}
 	
 }
--- src/game/world/SriksMap.java
+++ src/game/world/SriksMap.java
@@ -1,5 +1,8 @@
 package game.world;
 
+import game.TestObject;
+import game.objects.Table;
+
 public class SriksMap extends subgame
 {
 	public SriksMap()
@@ -10,6 +13,23 @@ public class SriksMap extends subgame
 		 * Then put them into the game with World.New(Object);
 		 */ 
 		
-		//I have no idea what I'm doing, help
+		//World.New(new Table (1f, 0f, 1f));
+		//World.New(new Table (2f, 0f, 2f));
+		//World.New(new Table (3f, 0f, 3f));
+		//World.New(new Table (-2f, 0f, 5f));
+		//World.New(new Table (3f, 0f, -3f));
+		//World.New(new Table (5f, 0f, -2f));
+		TestObject t = new TestObject(0f, 0, 1f);
+		t.setScale(.001f);
+		World.New(t);
+		World.New(new Table (-8f, 0f, 8f));
+		World.New(new Table (-6f, 0f, 6f));
+		World.New(new Table (-9f, 0f, 9f));
+		World.New(new Table (5f, 0f, -3f));
+		World.New(new Table (5f, 0f, -5f));
+		World.New(new Table (3f, 0f, 3f));
+		//World.New(new Table (1f, 0f, 1f));
+		//World.New(new Table (1f, 0f, 1f));
+		
 	}
 }
