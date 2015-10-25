777a1d872c120f5a81645da1ec39260f22080119
MasterMatthew <Mastamats@yahoo.com>
2015-10-24 20:45:21 -0400

    Jeremy's Map

---
+++ src/game/world/JeremMap.java
@@ -0,0 +1,13 @@
+package game.world;
+
+public class JeremMap extends subgame
+{
+	public JeremMap()
+	{
+		/*
+		 * Create objects such as Walls by using their constructor
+		 * Edit them however you'd like
+		 * Then put them into the game with World.New(Object);
+		 */
+	}
+}
