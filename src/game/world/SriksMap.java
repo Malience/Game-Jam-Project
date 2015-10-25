184cd4278f30325f99fe10d853cd4119efd2db6a
Uinstyn <Auraoic@gmail.com>
2015-10-24 20:55:04 -0400

    Merge branch 'master' of https://github.com/MasterMatthew/Game-Jam-Project

--- src/game/world/SriksMap.java
+++ src/game/world/SriksMap.java
@@ -9,5 +9,7 @@ public class SriksMap extends subgame
 		 * Edit them however you'd like
 		 * Then put them into the game with World.New(Object);
 		 */ 
+		
+		//I have no idea what I'm doing, help
 	}
 }
